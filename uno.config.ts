953b82aa07b811cc49d6f723730a6a91c9508efa
gaoqio <hi@gaoqio.com>
2024-10-24 23:16:31 +0800

    optimize header component

--- uno.config.ts
+++ uno.config.ts
@@ -9,11 +9,12 @@ import {
 export default defineConfig({
   shortcuts: {
     'switch-animation': 'transition duration-300',
-    'bg-base': 'bg-#e8e9eb dark:bg-#272624 switch-animation',
-    'bg-header': 'bg-#ffffff dark:bg-#000000 switch-animation',
-    'color-base': 'text-#363b45 dark:text-#C9C4CA switch-animation',
+    'bg-base': 'bg-#e8e9eb dark:bg-#161616 switch-animation',
+    'bg-header': 'bg-#ffffff dark:bg-#1a1a1a switch-animation',
+    'color-base': 'text-#363b45 dark:text-#878787 switch-animation',
     'color-header':
-      ' text-#878c95 dark:text-#979ca5 hover:text-#000000 dark:hover:text-#ffffff font-500 tracking-widest switch-animation',
+      ' text-#878c95 dark:text-#6c6c6c hover:text-#000000 dark:hover:text-#ffffff switch-animation',
+    'border-header': 'border-1 border-solid dark:border-#323232',
     'color-band': 'text-#000000 dark:text-#ffffff font-800 switch-animation',
     'color-icon': 'stroke-#000 dark:stroke-#fff',
   },
